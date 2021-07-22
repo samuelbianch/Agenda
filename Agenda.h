020ade7930646829e65ea026ae2d503609f58583
Samuel Bianch <69993163+samuelbianch@users.noreply.github.com>
2021-07-22 16:39:07 -0300

    Aa

--- Agenda.h
+++ Agenda.h
@@ -5,12 +5,14 @@ struct pessoa{
 	char telefone[11];
 	char email[40];
 	char cidade[40]; 
-	char estado[2]; 
+	char estado[2];
+	int atv;
 };
 
-typedef struct pessoa p;//tipo
+typedef struct pessoa *p;//tipo
 
-void leitura(p *a,int tam);
-void escrita(p *a,int tam);
-void escrita_nome_ordem_alfa_cres(p *a,int tam);
+void leitura(p a,int tam);
+void escrita(p a,int tam);
+void escrita_nome_ordem_alfa_cres(p a,int tam);
+void exclui(p a, int id);
 void menu();
